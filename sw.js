821aa1cf59cc27be68f081427ff718bbbc86d306
Abidu1 <abidusra@gmail.com>
2021-07-19 03:19:52 -0400

    fixing sw

--- sw.js
+++ sw.js
@@ -1,31 +1,3 @@
-
-//   let deferredPrompt;
-
-//   window.addEventListener('beforeinstallprompt', function(event) {
-//     // Prevent Chrome 67 and earlier from automatically showing the prompt
-//     event.preventDefault();
-//     // Stash the event so it can be triggered later.
-//     deferredPrompt = event;
-//   });
-
-//   // Installation must be done by a user gesture! Here, the button click
-//   btnAdd.addEventListener('click', (e) => {
-//     // hide our user interface that shows our A2HS button
-//     btnAdd.style.display = 'none';
-//     // Show the prompt
-//     deferredPrompt.prompt();
-//     // Wait for the user to respond to the prompt
-//     deferredPrompt.userChoice
-//       .then((choiceResult) => {
-//         if (choiceResult.outcome === 'accepted') {
-//           console.log('User accepted the A2HS prompt');
-//         } else {
-//           console.log('User dismissed the A2HS prompt');
-//         }
-//         deferredPrompt = null;
-//       });
-//   });
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('./sw.js').then(function(registration) {
@@ -60,20 +32,6 @@ self.addEventListener('install', function(event) {
         })
     );
   });
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       caches.match(event.request)
-//         .then(function(response) {
-//           // Cache hit - return response
-//           if (response) {
-//             return response;
-//           }
-//           return fetch(event.request);
-//         }
-//       )
-//     );
-//   });
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(

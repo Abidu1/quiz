b9b6347d227199b902199751025f14347a3e8d78
Abidu1 <abidusra@gmail.com>
2021-07-19 03:17:55 -0400

    adding sw

--- sw.js
+++ sw.js
@@ -1,3 +1,31 @@
+
+//   let deferredPrompt;
+
+//   window.addEventListener('beforeinstallprompt', function(event) {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     event.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = event;
+//   });
+
+//   // Installation must be done by a user gesture! Here, the button click
+//   btnAdd.addEventListener('click', (e) => {
+//     // hide our user interface that shows our A2HS button
+//     btnAdd.style.display = 'none';
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice
+//       .then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//           console.log('User accepted the A2HS prompt');
+//         } else {
+//           console.log('User dismissed the A2HS prompt');
+//         }
+//         deferredPrompt = null;
+//       });
+//   });
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('./sw.js').then(function(registration) {
@@ -80,17 +108,3 @@ self.addEventListener('fetch', function(event) {
         })
       );
   });
-
-  // Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});

4061edac5966e047eddd53d9b46d37b71a5f584c
Abidu1 <abidusra@gmail.com>
2021-07-19 01:56:51 -0400

    adding icons

--- sw.js
+++ sw.js
@@ -40,6 +40,20 @@ self.addEventListener('fetch', function(event) {
           if (response) {
             return response;
           }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
   
           return fetch(event.request).then(
             function(response) {
